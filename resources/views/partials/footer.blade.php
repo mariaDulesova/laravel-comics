0f4389f92e7c294f2ed6bd58db462cbfc461ffe3
mariaDulesova <maria.dulesova@gmail.com>
2021-07-13 17:00:24 +0200

    Footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,55 @@
+<footer>
+    <section>
+        <div class="section-links wrap">
+            <div class= "links ">
+                <div class="list-items">
+                    <h3>DC Comics</h3>
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                    <h3>Shop</h3>
+                    <ul>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+    
+                </div>
+                <div class="list-items">
+                    <h3>DC</h3>
+                    <ul>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy policy (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops </a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="list-items">
+                    <h3>Sites</h3>
+                    <ul>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+            </div>
+            <img src="{{asset('img/dc-logo-bg.png')}}" alt="">
+        </div> 
+    </section>
+    
+   @include('partials.signup')
+</footer>
---
+++ resources/views/partials/signup.blade.php
@@ -0,0 +1,15 @@
+<section class="section-sign">
+    <div class="bottom-links wrap">
+        <div class="button">
+            <a href="#">Sign-Up Now!</a>
+        </div>
+        <div class="socials">
+            <h3>Follow Us</h3>
+            <img src="{{asset('img/footer-facebook.png')}}" alt="Facebook">
+            <img src="{{asset('img/footer-twitter.png')}}" alt="Twitter">
+            <img src="{{asset('img/footer-youtube.png')}}" alt="Youtube">
+            <img src="{{asset('img/footer-pinterest.png')}}" alt="Pinterest">
+            <img src="{{asset('img/footer-periscope.png')}}" alt="Periscope">
+        </div>
+    </div>
+</section>
