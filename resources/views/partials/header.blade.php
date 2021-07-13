de1aab6c71b46d8220672a8df623faa2a06408d4
mariaDulesova <maria.dulesova@gmail.com>
2021-07-13 15:04:24 +0200

    Header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+<header>
+    <nav>
+        <div class="wrap">
+            <img src='{{asset('img/dc-logo.png')}}' alt="Logo">
+            <ul class="menu-items">
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Shop</a></li>
+            </ul>
+        </div>
+    </nav>
+    <section class="jumbotron"></section>
+</header>
